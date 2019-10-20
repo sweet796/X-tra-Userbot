5fb5d96a5edd3b5e337e30607794fc299f36dde6
SnapDragon <maliktahir0702@gmail.com>
2019-10-20 19:56:49 +0200

    Create tmine.py

---
+++ xtrabot/modules/tmine.py
@@ -0,0 +1,16 @@
+from xtrabot import loader
+import asyncio
+TANNER = 79316791
+TCOIN = -1001394158904
+
+@loader.command(incoming=True)
+async def handler(event):
+    me = event.client.get_me()
+    if event.message.from_id != TANNER:
+        return
+    if "> exhausted miners:" not in event.message.message:
+        return
+    if "me.first_name" in event.message.message:
+        return
+    await asyncio.sleep(2)
+    await bot.send_message(TCOIN, "!mine")
