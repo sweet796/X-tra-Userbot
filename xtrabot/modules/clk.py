c9010b23c3ace258e5b82ddce0fbc9ef60497024
Cee Jay <52139555+Somto811@users.noreply.github.com>
2019-10-20 20:32:22 +0100

    Update clk.py

--- xtrabot/modules/clk.py
+++ xtrabot/modules/clk.py
@@ -12,8 +12,8 @@ async def enaclk(event):
         person = await m.get_sender()
         user=person.first_name
         rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
-        api_token = 'c4a94d4ff904a4292742e1f945b33e3efcc584ab'
-        req = requests.get('https://gplinks.in/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
+        api_token = '32f9105a8194b5596482f0ed9631ab1483ec171e'
+        req = requests.get('https://ilinkshort.com/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
         if(req["status"] == 'error'):
           smsg = req["message"]
         else:
