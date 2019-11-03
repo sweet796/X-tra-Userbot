9690011c03a93979c2e6a4aa8ec38aa8498b0d87
sweet796 <54498707+sweet796@users.noreply.github.com>
2019-11-03 01:11:42 +0100

    Update clk.py

--- xtrabot/modules/clk.py
+++ xtrabot/modules/clk.py
@@ -12,7 +12,7 @@ async def enaclk(event):
         person = await m.get_sender()
         user=person.first_name
         rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
-        api_token =)'b43f9c1d53d8a74c639628d193f58ea797b23184'
+        api_token = 'b43f9c1d53d8a74c639628d193f58ea797b23184'
         req = requests.get('https://ptlinks.in/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
         if(req["status"] == 'error'):
           smsg = req["message"]
